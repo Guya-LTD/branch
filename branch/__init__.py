eda12c34f6af13d9cc62125b98c399df437f6bca
semantic-release <semantic-release>
2020-09-26 03:38:39 +0000

    0.3.5
    Automatically generated by python-semantic-release

--- branch/__init__.py
+++ branch/__init__.py
@@ -54,7 +54,7 @@ from .handlers import register_handler
 from branch.controller.v1 import *
 
 # const vars
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 __email__ = 'simonbelete@gmail.com'
 __title__ = 'Branch Service'
 __author__ = 'Simon Belete'
