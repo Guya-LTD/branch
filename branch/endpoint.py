082940055cc20c69167f131bded05ee673f33aa5
Simonbelete <simonbelete@gmail.com>
2020-10-25 08:50:35 +0300

    feat(endpoint) gatekeepre endpoint

---
+++ branch/endpoint.py
@@ -0,0 +1,40 @@
+"""Copyright Header Details
+
+Copyright
+---------
+    Copyright (C) Guya , PLC - All Rights Reserved (As Of Pending...)
+    Unauthorized copying of this file, via any medium is strictly prohibited
+    Proprietary and confidential
+
+LICENSE
+-------
+    This file is subject to the terms and conditions defined in
+    file 'LICENSE.txt', which is part of this source code package.
+
+Authors
+-------
+    * [Simon Belete](https://github.com/Simonbelete)
+ 
+Project
+-------
+    * Name: 
+        - Guya E-commerce & Guya Express
+    * Sub Project Name:
+        - Branch Service
+    * Description
+        - Branch location and details service
+"""
+
+
+"""This module contains class whose instances will be used to
+load the settings according to the running environment. """
+
+import os
+
+class Endpoint:
+    def gatekeeper(self, name):
+        if(os.environ.get('IS_MOCKING')):
+            return 'http://' + os.environ.get('MOCKING_SERVER_URL') + "/api/v1/" + name
+        elif(not os.environ.get('IS_MOCKING')):
+            return 'http://' + os.environ.get('GATEKEEPER_URL') + os.environ.get('GATEKEEPER_PORT') + "/api/v1/" + name
+
